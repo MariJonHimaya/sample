0311ceec703121471f64ced27ff7279fe1b44c1a
MariJonHimaya <himayamarijon@gmail.com>
2023-10-27 08:10:15 +0800

    first commit

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ $title}}</title>
+	<link href="" rel="stylesheet" integrity="" crossorigin="" >
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+<script src=""></script>
+
+</body>
+</html>
